c84be31504e41cb7f6ce2377a6985190c9d5141b
Max Glass <glassmax13@gmail.com>
2021-06-11 11:17:03 -0400

    ByteBuffer: Make position be an Optional value
    If the buffer is uninitialized, representing the position as 0
    doesn't make sense and `None` is much more apt.

--- src/byte_buffer.rs
+++ src/byte_buffer.rs
@@ -13,21 +13,21 @@ pub(crate) trait FromByteBuffer: Sized {
 }
 
 /// Represents a stream of bytes and allows for conversion to and from different types.
-/// Internally keeps track of position in the buffer to allow for behavior similiar to `Iterators`.
+/// Internally keeps track of position in the buffer to allow for behavior similar to `Iterators`.
 ///
 /// Differs from [`Vec`] in terms of memory as all memory allocations are explicit using functions like [`with_size`] or [`resize`]
 ///
-/// Used in conjuction with [`FromByteBuffer`].
+/// Used in conjunction with [`FromByteBuffer`].
 ///
 /// [`with_size`]: crate::byte_buffer::ByteBuffer::with_size
 /// [`resize`]: crate::byte_buffer::ByteBuffer::resize
 pub(crate) struct ByteBuffer {
     buffer: Vec<u8>,
-    position: usize,
+    position: Option<usize>,
 }
 
 impl ByteBuffer {
-    /// Constructs a new, empty `ByteBuffer` at position 0.
+    /// Constructs a new, empty `ByteBuffer` with an uninitialized position
     ///
     /// Does not allocate until told to with [`resize`]
     ///
@@ -35,15 +35,19 @@ impl ByteBuffer {
     pub(crate) fn new() -> ByteBuffer {
         ByteBuffer {
             buffer: Vec::new(),
-            position: 0,
+            position: None,
         }
     }
 
     /// Constructs a new `ByteBuffer` with the specified size at position 0.
     pub(crate) fn with_size(size: usize) -> ByteBuffer {
+        if size == 0 {
+            return ByteBuffer::new();
+        }
+
         ByteBuffer {
             buffer: Vec::with_capacity(size),
-            position: 0,
+            position: Some(0),
         }
     }
 
@@ -55,8 +59,13 @@ impl ByteBuffer {
     ///
     /// This function is very expensive.
     pub(crate) fn resize(&mut self, new_size: usize) {
-        if self.position >= new_size {
-            self.position = new_size - 1;
+        // make sure the `position` is updated properly
+        if new_size == 0 {
+            self.position = None;
+        } else if self.position == None {
+            self.position = Some(0);
+        } else if self.position.unwrap() >= new_size {
+            self.position = Some(new_size - 1);
         }
 
         let mut new_buf = Vec::with_capacity(new_size);
@@ -70,41 +79,49 @@ impl ByteBuffer {
         self.buffer = new_buf;
     }
 
-    /// Gets the number of bytes not yet consumed - calculated off the current internal position
+    /// Gets the number of bytes not yet consumed - calculated off the current internal position.
+    ///
+    /// If the position is uninitialized returns 0.
     pub(crate) fn bytes_not_consumed(&self) -> usize {
-        self.buffer.len() - self.position
+        self.buffer.len() - self.position.unwrap_or_else(|| self.buffer.len())
     }
 
     /// Used with the [`FromByteBuffer`] trait to try to consume a buffer and construct the generically specified type
     ///
-    /// Returns `None` if it out of bounds of the buffer's capacity or hasn't filled that far.
+    /// Returns `None` if it out of bounds or the buffer hasn't been filled to the position yet.
     /// Also if the type is not encoded in the buffer properly.
     pub(crate) fn consume<T: FromByteBuffer>(&mut self) -> Option<T> {
-        T::from_byte_buffer(self)
+        let out = T::from_byte_buffer(self);
+
+        if let Some(_) = out {
+            self.position = Some(self.position? + T::size_from_byte_buffer(self)?);
+        }
+        out
     }
 
     /// Consume the passed amount of bytes
     ///
-    /// Returns `None` if it out of bounds of the buffer's capacity or hasn't filled that far.
+    /// Returns `None` if it out of bounds or the buffer hasn't been filled to the position yet.
     pub(crate) fn consume_bytes(&mut self, num_bytes: usize) -> Option<&[u8]> {
-        let out = self.buffer.get(self.position..self.position + num_bytes);
+        let position = self.position?;
+        let out = self.buffer.get(position..position + num_bytes);
 
         if let Some(_) = out {
-            self.position += num_bytes;
-            return out;
+            self.position = Some(position + num_bytes);
         }
-        None
+        out
     }
 
-    /// Peeking is similiar to consuming except it doen't increment the internal position.
+    /// Peeking is similar to consuming except it don't increment the internal position.
     /// Because it doesn't increment the internal position it doesn't effectively consume the bytes, hence why it "peeks."
     ///
-    /// Returns `None` if it out of bounds of the buffer's capacity or hasn't filled that far.
+    /// Returns `None` if it out of bounds or the buffer hasn't been filled to the position yet.
     pub(crate) fn peek_bytes(&self, num_bytes: usize) -> Option<&[u8]> {
-        self.buffer.get(self.position..self.position + num_bytes)
+        let position = self.position?;
+        self.buffer.get(position..position + num_bytes)
     }
 
-    /// Similiar to [`peek_bytes`] except it guarantees that it returns a valid buffer.
+    /// Similar to [`peek_bytes`] except it guarantees that it returns a valid buffer.
     /// This is possible because it will return either up to the amount of bytes specified or until the end of the bound of the buffer, whichever comes first.
     ///
     /// [`peek_bytes`]: crate::byte_buffer::ByteBuffer::peek_bytes
